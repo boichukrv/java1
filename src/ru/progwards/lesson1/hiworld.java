d6801e15f9f9e81eb981244d4b6c0343568ede3e
roman <boichukrv@gmail.com>
2019-11-13 11:50:04 +0200

    Revert "new wrong class"
    This reverts commit 8ae682c3

--- src/ru/progwards/lesson1/hiworld.java
+++ src/ru/progwards/lesson1/hiworld.java
@@ -1,13 +1,6 @@
 package ru.progwards.lesson1;
 public class hiworld {
-    static int addition (int x, int y){
-        return x+y;
-    }
-    static int addition1 (int x, int y){
-        return x+y;
-    }
 public static void main (String[] args){
     System.out.println("Hello world");
-    System.out.print(addition (7,5));
 }
 }
