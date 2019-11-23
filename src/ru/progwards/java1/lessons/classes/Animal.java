9d19303cae95365fbcfc89a743eda1aa5f73f9d2
roman <boichukrv@gmail.com>
2019-11-23 15:17:16 +0200

    домашняя работа H4

--- src/ru/progwards/java1/lessons/classes/Animal.java
+++ src/ru/progwards/java1/lessons/classes/Animal.java
@@ -29,10 +29,14 @@ double weight;
     return "I am "+getKind()+", eat "+getFoodKind();
         }
 
+    public double getWeight() {
+        return weight;
+    }
+
     public double getFoodCoeff(){
         return 0.02;
     }
-    public double calculateFoodWeight(){return weight*getFoodCoeff();}
+    public double calculateFoodWeight(){return getweight()*getFoodCoeff();}
 
     public String toStringFull(){ return "I am " +getKind()+" eat "+getFoodKind()+" "+calculateFoodWeight();}
 
