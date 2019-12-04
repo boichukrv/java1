1cf75c9aedd33b4b1e727829a4bfbd8e2b6f6f36
Roman <boichukrv@gmail.com>
2019-12-04 16:22:16 +0200

    homework 6

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -14,6 +14,7 @@ public class DIntArray {
         int[] temp = Arrays.copyOf(set, set.length + 1);
         set = temp;
         set[set.length - 1] = num;
+        System.out.println(Arrays.toString(set));
     }
 
     public void atInsert(int pos, int num) {
@@ -22,16 +23,36 @@ public class DIntArray {
         temp[pos - 1] = num;
         System.arraycopy(set, pos - 1, temp, pos, (set.length - pos + 1));
         set = temp;
+        System.out.println(Arrays.toString(set));
     }
 
     public void atDelete(int pos) {
         int[] temp = new int[set.length - 1];
-        System.arraycopy(set, 0, temp, 0, pos);
-        System.arraycopy(set, pos, temp, pos - 1, (set.length - pos));
+        System.arraycopy(set, 0, temp, 0, pos - 1);
+        System.arraycopy(set, pos, temp, pos, (set.length - pos));
         set = temp;
+        System.out.println(Arrays.toString(set));
     }
 
     public int at(int pos) {
-        return set[pos];
+        System.out.println(set[pos-1]);
+        return set[pos-1];
     }
+
+
+    public static void main(String[] args) {
+        DIntArray arr = new DIntArray();
+        arr.add(-5);
+        arr.add(5);
+        arr.add(4);
+        arr.add(7);
+        arr.add(8);
+        arr.add(23);
+        arr.add(-54);
+        arr.add(0);
+        arr.add(40);
+        arr.atInsert(2, 6);
+        arr.at(2);
+    }
+
 }
